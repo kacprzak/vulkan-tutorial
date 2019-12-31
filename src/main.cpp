b587ea7aab42d434feea5c96c503e8c704cfcdff
Marcin Kacprzak <martin.kacprzak@gmail.com>
2019-12-31 14:51:31 +0100

    Created graphics pipeline

--- src/main.cpp
+++ src/main.cpp
@@ -568,6 +568,28 @@ class HelloTriangleApplication
             VK_SUCCESS)
             throw std::runtime_error{"failed to create pipeline layout!"};
 
+        VkGraphicsPipelineCreateInfo pipelineInfo = {};
+        pipelineInfo.sType                        = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
+        pipelineInfo.stageCount                   = 2;
+        pipelineInfo.pStages                      = shaderStages;
+        pipelineInfo.pVertexInputState            = &vertexInputInfo;
+        pipelineInfo.pInputAssemblyState          = &inputAssembly;
+        pipelineInfo.pViewportState               = &viewportState;
+        pipelineInfo.pRasterizationState          = &rasterizer;
+        pipelineInfo.pMultisampleState            = &multisampling;
+        pipelineInfo.pDepthStencilState           = nullptr; // Optional
+        pipelineInfo.pColorBlendState             = &colorBlending;
+        pipelineInfo.pDynamicState                = &dynamicState;
+        pipelineInfo.layout                       = m_pipelineLayout;
+        pipelineInfo.renderPass                   = m_renderPass;
+        pipelineInfo.subpass                      = 0;
+        pipelineInfo.basePipelineHandle           = VK_NULL_HANDLE;
+        pipelineInfo.basePipelineIndex            = -1;
+
+        if (vkCreateGraphicsPipelines(m_device, VK_NULL_HANDLE, 1, &pipelineInfo, nullptr,
+                                      &m_graphicsPipeline) != VK_SUCCESS)
+            throw std::runtime_error{"failed to create graphics pipeline!"};
+
         vkDestroyShaderModule(m_device, fragShaderModule, nullptr);
         vkDestroyShaderModule(m_device, vertShaderModule, nullptr);
     }
@@ -595,6 +617,7 @@ class HelloTriangleApplication
 
     void cleanup()
     {
+        vkDestroyPipeline(m_device, m_graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(m_device, m_pipelineLayout, nullptr);
         vkDestroyRenderPass(m_device, m_renderPass, nullptr);
 
@@ -624,6 +647,7 @@ class HelloTriangleApplication
     std::vector<VkImageView> m_swapChainImageViews;
     VkRenderPass m_renderPass;
     VkPipelineLayout m_pipelineLayout;
+    VkPipeline m_graphicsPipeline;
 };
 
 int main()
