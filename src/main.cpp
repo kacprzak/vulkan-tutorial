391d127a68a299ec77dd4f4480633e2653fe0bc3
Marcin Kacprzak <mkacprzak@novomatic-tech.com>
2024-01-01 16:46:32 +0100

    RenderDoc requires Vulkan 1.2

--- src/main.cpp
+++ src/main.cpp
@@ -211,7 +211,7 @@ class HelloTriangleApplication
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName        = "No Engine";
         appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion         = VK_API_VERSION_1_0;
+        appInfo.apiVersion         = VK_API_VERSION_1_2;
 
         printAvailableExtensions();
 
